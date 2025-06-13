42dbde6ed02492ba56e57c161327b2946fa7811d
nelsonjhongp <nelsonjhongp@gmail.com>
2025-06-12 23:02:36 -0500

    practica arreglos desplazamiento

---
+++ src/Seccion12_Arreglos/PracticaArreglosDesplazarPosicion03.java
@@ -0,0 +1,36 @@
+package Seccion12_Arreglos;
+
+import java.util.Scanner;
+
+public class PracticaArreglosDesplazarPosicion03 {
+    public static void main(String[] args) {
+        int [] a = new int[7];
+        int numero, posicion;
+
+        Scanner s = new Scanner(System.in);
+
+        for (int i = 0; i < a.length - 1; i++){
+            System.out.print("Ingrese un numero: ");
+            a[i] = s.nextInt();
+        }
+        System.out.println();
+
+        System.out.println("Ingrese un numero a insertar: ");
+        numero = s.nextInt();
+
+        posicion = 0;
+        while (posicion < 6 && numero > a[posicion]){
+            posicion++;
+        }
+
+        for (int i = a.length - 2; i >= posicion; i--){
+            a[i+1] = a[i];
+        }
+
+        a[posicion] = numero;
+        System.out.println("El nuevo arreglo ordenado: ");
+        for (int i = 0; i < a.length; i++){
+            System.out.println(i + " => " + a[i]);
+        }
+    }
+}
