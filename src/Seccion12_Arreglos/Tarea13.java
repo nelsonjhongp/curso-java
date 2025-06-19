5653bffc27f15c2cccc0d096a9a775d106c96372
nelsonjhongp <nelsonjhongp@gmail.com>
2025-06-18 23:40:39 -0500

    noo gente

--- src/Seccion12_Arreglos/Tarea13.java
+++ src/Seccion12_Arreglos/Tarea13.java
@@ -26,6 +26,6 @@ public class Tarea13 {
         for(int i = 0; i < b.length; i++){
             System.out.println(b[i]);
         }
-        //hola gentita
+        //hola gentita nooo otra vez porque
     }
 }
