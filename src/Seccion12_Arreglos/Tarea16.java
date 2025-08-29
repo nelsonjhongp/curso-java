5d4ed12db69f836f5f65d1af76946f3e11a857a9
nelsonjhongp <nelsonjhongp@gmail.com>
2025-08-28 23:58:43 -0500

    mejorando codigo

---
+++ src/Seccion12_Arreglos/Tarea16.java
@@ -0,0 +1,16 @@
+package Seccion12_Arreglos;
+
+import java.util.Scanner;
+
+public class Tarea16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arreglo = new int[10];
+
+        System.out.println("Ingrese 10 numeros enteros (1-9).");
+        for (int i = 0; i < arreglo.length; i++) {
+
+        }
+    }
+}
