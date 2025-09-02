f9474cf60b78453bbec914d5f3d1e7cde1bd8157
nelsonjhongp <nelsonjhongp@gmail.com>
2025-09-01 23:59:25 -0500

    actualizando codigo tarea 16

--- src/Seccion12_Arreglos/Tarea16.java
+++ src/Seccion12_Arreglos/Tarea16.java
@@ -7,14 +7,34 @@ public class Tarea16 {
         Scanner scanner = new Scanner(System.in);
 
         int[] arreglo = new int[10];
+        int[] contador = new int[10];
 
-        System.out.println("Ingrese 10 numeros enteros (1-9).");
+        System.out.println("Ingrese 10 numeros enteros (1-9):");
         for (int i = 0; i < arreglo.length; i++) {
+            System.out.print("Numero " + (i + 1) + ": ");
             int numero = scanner.nextInt();
-            if (numero > 9 && numero < 1){
+
+            if (numero < 1 || numero > 9) {
                 System.out.println("El numero no esta dentro del rango (1-9).");
                 i--;
+            } else {
+                arreglo[i] = numero;
+                contador[numero]++;
             }
         }
+
+        int numeroMasRepite = 0;
+        int maxOcurrencias = 0;
+
+        for (int i = 1; i <= 9; i++) {
+            if (contador[i] > maxOcurrencias) {
+                maxOcurrencias = contador[i];
+                numeroMasRepite = i;
+            }
+        }
+
+        System.out.println("\nResultado:");
+        System.out.println("El numero que mas se repite es " + numeroMasRepite +
+                " con " + maxOcurrencias + " ocurrencias.");
     }
 }
